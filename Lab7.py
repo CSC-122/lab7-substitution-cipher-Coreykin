941cbf9aa4e20e4152f5b38e58a2604616045f3e
Coreykin <169112633+Coreykin@users.noreply.github.com>
2024-06-16 01:07:37 -0400

    Update Lab7.py

--- Lab7.py
+++ Lab7.py
@@ -9,10 +9,20 @@ Input(s):
 Processes:
 Output(s):
 """
-
 def main():
-    pass # replace this line with all of your solution code
+    # Encrypt the message
+    message = input("Enter the message to be encrypted: ")
+    encrypted_message = ""
+    for char in message:
+        encrypted_message += chr(ord(char) + 3)
+    print(f"Encrypted message = {encrypted_message}\n")
 
+    # Decrypt the message
+    cipher = input("Enter the cipher to be decrypted: ")
+    decrypted_message = ""
+    for char in cipher:
+        decrypted_message += chr(ord(char) - 3)
+    print(f"Decrypted message = {decrypted_message}\n")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
